4cbddc2b321d62b51424209c7a421ceb0af7b5de
iamLusani <lusani.22mphigalale@gmail.com>
2024-06-14 19:56:08 +0200

    removed everything begin again with footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import Weather from "./Weather";
-
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App  container ">
-      <h1>
-        {" "}
-        <Weather defualtCity="Durban" />
-      </h1>
+    <div className="App">
+      {" "}
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/iamLusani">Lusani</a>.{" "}
+        <a href="https://github.com/iamLusani/react-weather-app">
+          Open-sourcd on git-hub
+        </a>{" "}
+        and{" "}
+        <a href="https://advance-weather-app-react.netlify.app/">
+          hosted on netlify{" "}
+        </a>
+      </footer>{" "}
     </div>
   );
 }
